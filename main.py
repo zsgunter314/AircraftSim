d38f4aee3c45fdf601b292247f1d14cc6e2b75b7
zsgunter314 <zsg0205@utulsa.edu>
2024-01-19 11:32:10 -0600

    First day changes
    added .gitignore and example code in main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import numpy as np
+from matplotlib import pyplot as plt
 
+x = np.arange(0,20,1)
+
+y = 2 * x + 5
+plt.title("Matplotlib demo") 
+plt.xlabel("x axis caption") 
+plt.ylabel("y axis caption") 
+plt.plot(x,y)
+plt.show()
+
+
+print("Hello World")
