113616e00c24a1d820b3dce847ad08fd05c64802
zsgunter314 <zsg0205@utulsa.edu>
2024-01-29 11:44:32 -0600

    Setup the view_MAV and draw_mav
    see above

--- viewers/draw_mav.py
+++ viewers/draw_mav.py
@@ -1,1 +1,135 @@
-Copy and paste draw_spacecraft.py here and then edit to draw a UAV instead of the spacecraft.
+"""
+mavsim_python: drawing tools
+    - Beard & McLain, PUP, 2012
+    - Update history:
+        1/13/2021 - TWM
+        7/13/2023 - RWB
+        1/16/2024 - RWB
+"""
+import numpy as np
+import pyqtgraph.opengl as gl
+from tools.rotations import euler_to_rotation
+from tools.drawing import rotate_points, translate_points, points_to_mesh
+
+
+class DrawMAV:
+    def __init__(self, state, window, scale=10):
+        """
+        Draw the MAV.
+
+        The input to this function is a (message) class with properties that define the state.
+        The following properties are assumed:
+            state.north  # north position
+            state.east  # east position
+            state.altitude   # altitude
+            state.phi  # roll angle
+            state.theta  # pitch angle
+            state.psi  # yaw angle
+        """
+        self.unit_length = scale
+        sc_position = np.array([[state.north], [state.east], [-state.altitude]])  # NED coordinates
+        # attitude of mav as a rotation matrix R from body to inertial
+        R_bi = euler_to_rotation(state.phi, state.theta, state.psi)
+        # convert North-East Down to East-North-Up for rendering
+        self.R_ned = np.array([[0, 1, 0], [1, 0, 0], [0, 0, -1]])
+        # get points that define the non-rotated, non-translated MAV and the mesh colors
+        self.sc_points, self.sc_index, self.sc_meshColors = self.get_sc_points()
+        self.sc_body = self.add_object(
+            self.sc_points,
+            self.sc_index,
+            self.sc_meshColors,
+            R_bi,
+            sc_position)
+        window.addItem(self.sc_body)  # add MAV to plot     
+
+    def update(self, state):
+        sc_position = np.array([[state.north], [state.east], [-state.altitude]])  # NED coordinates
+        # attitude of mav as a rotation matrix R from body to inertial
+        R_bi = euler_to_rotation(state.phi, state.theta, state.psi)
+        self.sc_body = self.update_object(
+            self.sc_body,
+            self.sc_points,
+            self.sc_index,
+            self.sc_meshColors,
+            R_bi,
+            sc_position)
+
+    def add_object(self, points, index, colors, R, position):
+        rotated_points = rotate_points(points, R)
+        translated_points = translate_points(rotated_points, position)
+        translated_points = self.R_ned @ translated_points
+        # convert points to triangular mesh defined as array of three 3D points (Nx3x3)
+        mesh = points_to_mesh(translated_points, index)
+        object = gl.GLMeshItem(
+            vertexes=mesh,  # defines the triangular mesh (Nx3x3)
+            vertexColors=colors,  # defines mesh colors (Nx1)
+            drawEdges=True,  # draw edges between mesh elements
+            smooth=False,  # speeds up rendering
+            computeNormals=False)  # speeds up rendering
+        return object
+
+    def update_object(self, object, points, index, colors, R, position):
+        rotated_points = rotate_points(points, R)
+        translated_points = translate_points(rotated_points, position)
+        translated_points = self.R_ned @ translated_points
+        # convert points to triangular mesh defined as array of three 3D points (Nx3x3)
+        mesh = points_to_mesh(translated_points, index)
+        object.setMeshData(vertexes=mesh, vertexColors=colors)
+        return object
+
+    def get_sc_points(self):
+        """"
+            Points that define the MAV, and the colors of the triangular mesh
+            Define the points on the MAV following information in Appendix C.3
+        """
+        # points are in XYZ coordinates
+        #   define the points on the MAV according to Appendix C.3
+        points = self.unit_length * np.array([
+            [1, 1, 0],  # point 1 [0]
+            [1, -1, 0],  # point 2 [1]
+            [-1, -1, 0],  # point 3 [2]
+            [-1, 1, 0],  # point 4 [3]
+            [1, 1, -2],  # point 5 [4]
+            [1, -1, -2],  # point 6 [5]
+            [-1, -1, -2],  # point 7 [6]
+            [-1, 1, -2],  # point 8 [7]
+            [1.5, 1.5, 0],  # point 9 [8]
+            [1.5, -1.5, 0],  # point 10 [9]
+            [-1.5, -1.5, 0],  # point 11 [10]
+            [-1.5, 1.5, 0]  # point 12 [11]
+            ]).T
+        # point index that defines the mesh
+        index = np.array([
+            [0, 1, 5],  # front 1
+            [0, 5, 4],  # front 2
+            [3, 2, 6],  # back 1
+            [3, 6, 7],  # back 2
+            [0, 4, 7],  # right 1
+            [0, 7, 3],  # right 2
+            [1, 5, 6],  # left 1
+            [1, 6, 2],  # left 2
+            [4, 5, 6],  # top 1
+            [4, 6, 7],  # top 2
+            [8, 9, 10],  # bottom 1
+            [8, 10, 11],  # bottom 2  
+            ])
+        #   define the colors for each face of triangular mesh
+        red = np.array([1., 0., 0., 1])
+        green = np.array([0., 1., 0., 1])
+        blue = np.array([0., 0., 1., 1])
+        yellow = np.array([1., 1., 0., 1])
+        meshColors = np.empty((13, 3, 4), dtype=np.float32)
+        meshColors[0] = yellow  # front 1
+        meshColors[1] = yellow  # front 2
+        meshColors[2] = yellow  # back 1
+        meshColors[3] = yellow  # back 2
+        meshColors[4] = blue  # right 1
+        meshColors[5] = blue  # right 2
+        meshColors[6] = blue  # left 1
+        meshColors[7] = blue  # left 2
+        meshColors[8] = red  # top 1
+        meshColors[9] = red  # top 2
+        meshColors[10] = green  # bottom 1
+        meshColors[11] = green  # bottom 2
+        return points, index, meshColors
+
